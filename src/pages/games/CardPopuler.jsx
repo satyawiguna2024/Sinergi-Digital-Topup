6e32e0a2ed06bb5967261f55768b0900351b39b2
I Made Satya Wiguna <satwig600@gmail.com>
2025-09-10 21:50:28 +0800

    modified game populer

--- src/pages/games/CardPopuler.jsx
+++ src/pages/games/CardPopuler.jsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router";
+import data from "../../json/data.json";
+
 export default function CardPopuler() {
+
+  const populer = [data[0], data[1], data[2], data[3]];
   return (
     <>
       <div className="mx-2 overflow-x-auto">
         <div className="grid grid-flow-col auto-cols-[130px] gap-x-3">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((_, index) => (
-            <div
+          {populer.map((itm, index) => (
+            <Link
+              to={`/games/topup/${itm.id}`}
               key={index}
-              className="w-full h-[170px] bg-slate-200 rounded-lg mt-2 flex justify-center items-center"
-            >{index + 1}</div>
+              className="w-full h-[170px] rounded-lg mt-2 flex justify-center items-center"
+            >
+              <img src={itm.image} alt={itm.name} className="size-full object-cover bg-cover rounded-lg" />
+            </Link>
           ))}
         </div>
       </div>
